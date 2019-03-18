4fe4839e40c490bd64452255968207a593e13ae3
qbcs <qbcs@9fbank.cc>
2019-03-18 11:34:10 +0800

    :

---
+++ README.md
@@ -0,0 +1,16 @@
+# Excel_Convert_To_TestLink
+#很多年前写的工具，有同学在qq上问我，就搬过来了，顺带改了个Bug
+把Excel测试用例转换为可以导入testlink格式的转换工具
+
+#windows用户
+### 1.解压工具    TestLink_XLSX_TO_XML.zip
+### 2.将现有Excel用例，复制到模板excel中
+### 3.执行bat，进行转换
+### 4.在testlink中导入即可
+![cmd-markdown-logo](https://i.niupic.com/images/2019/03/14/5RgU.png)
+
+#Mac用户
+### 1.解压工具    TestLink_XLSX_TO_XML.zip
+### 2.将现有Excel用例，复制到模板excel中
+### 3.直接运行jar文件
+### 4.在testlink中导入即可
---
+++ lottery/js/users.js
@@ -0,0 +1,28 @@
+var members = [
+    {
+        "fileName": "liuwenbin.jpg",
+        "memberName": "刘文彬",
+        "fullName": "呵呵哒 刘文彬"
+    },
+    {
+        "fileName": "hehe.jpg",
+        "memberName": "呵呵",
+        "fullName": "嘿嘿嘿 呵呵"
+    },
+    {
+        "fileName": "mali.jpg",
+        "memberName": "玛丽",
+        "fullName": "嘿嘿嘿 玛丽"
+    },
+    {
+        "fileName": "peiqi.jpg",
+        "memberName": "佩奇",
+        "fullName": "嘿嘿 佩奇"
+    },
+    {
+        "fileName": "qiongren.jpg",
+        "memberName": "穷人",
+        "fullName": "嘿嘿嘿 穷人"
+    }
+
+    ];
